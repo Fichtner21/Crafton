440b6c75677712284bde14c36c82b0033e9bbd9e
POZ-ERNESTF\e_fichtner <e.fichtner@sputnik.pl>
2019-03-17 00:50:48 +0100

    stopka, slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	console.log('test');
-})
+	//SLIDER
+
+	$(function(){
+      	$('.bxslider').bxSlider({
+	        mode: 'fade',
+	        captions: true,
+	        slideWidth: 600
+      	});
+  	});
+
+  	var marker = new google.maps.Marker({
+      	position: map.getCenter(),
+      	icon: 'images/pin-maker.png',
+      	map: map
+    });
+
+});
