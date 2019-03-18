28d46daa5ae47b3df6eea4e2ee33f2c45608b982
POZ-ERNESTF\e_fichtner <e.fichtner@sputnik.pl>
2019-03-18 01:10:05 +0100

    sticky menu, sekcje style

--- js/main.js
+++ js/main.js
@@ -9,10 +9,53 @@ $(document).ready(function(){
       	});
   	});
 
-  	var marker = new google.maps.Marker({
-      	position: map.getCenter(),
-      	icon: 'images/pin-maker.png',
-      	map: map
-    });
+  	var stickyNavTop = $('.header-nav').offset().top;
+  	console.log(stickyNavTop);
+  	timeout = '';
+
+  	function stickyNav(){
+  		var scrollTop = $(window).scrollTop();
+  		//console.log(scrollTop);
+  		if (scrollTop > stickyNavTop) {
+  			$('.header-nav').addClass('sticky');
+  			
+  			//if(timeout){
+  				//clearTimeout(timeout);
+  				//timeout = null;
+  			//}
+  			//console.log('dodalo klasy sticky');
+  		} else {
+  			//timeout = setTimeout(function(){
+  				$('.header-nav').removeClass('sticky').addClass('trans');
+  				
+  			//},1000);
+  			
+  			//console.log('usunieto klase sticky');
+  		}
+  	}
+
+  	stickyNav();
+	// and run it again every time you scroll
+	$(window).scroll(function() {
+		stickyNav();
+	});
+
+	
+
+	$(window).scroll(function(){
+		var hT = $('.section').offset().top,
+			hH = $('.section').outerHeight(),
+			wH = $(window).height(),
+			wS = $(this).scrollTop();
+			console.log('hT: ' + hT + ' hH: ' + hH + ' wH: ' + wH + ' wS: ' + wS);
+		if(wS > (hT+hH-wH)){
+			$('.img').addClass('scale-plus');
+			console.log('doszlo');
+		} 
+		// if (wS > (hT+hH-wH)) {
+		// 	$('.img').removeClass('scale-plus');
+		// }
+		
+	})  	
 
 });
